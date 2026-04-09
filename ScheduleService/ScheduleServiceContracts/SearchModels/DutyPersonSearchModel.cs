d9d6b8b8bf6a4624d71a35b83edb3627392268ed
bulatova_karina <midnight737475@gmail.com>
2026-04-09 23:58:35 +0400

    я хочу пить джус у бассейна а не вот это все

--- ScheduleService/ScheduleServiceContracts/SearchModels/DutyPersonSearchModel.cs
+++ ScheduleService/ScheduleServiceContracts/SearchModels/DutyPersonSearchModel.cs
@@ -11,6 +11,7 @@ namespace ScheduleServiceContracts.SearchModels
         public int? Id { get; set; }
         public string? LastName { get; set; }
         public string? FirstName { get; set; }
+        public string? Position { get; set; }
         public string? Phone { get; set; }
         public string? Email { get; set; }
     }
